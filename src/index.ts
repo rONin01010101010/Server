5f0ba7b1f7fa392760388ba5fb93dd096db96c75
rONin01010101010 <jkenan72@gmail.com>
2026-09-09 13:29:50 -0400

    ran basic server, added index.html to serve file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express , {type Express , type Request, type Response} from 'express'
+
+const app: Express = express()
+const port = 8080
+
+app.use(express.static('.'))
+
+app.listen(port, () => {
+   console.log(`Listening on port ${port}`) 
+})
